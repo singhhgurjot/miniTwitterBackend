41124654f550cae044b45eab30261c516749fcca
Gurjot Singh <gurjot12111@gmail.com>
2024-06-10 16:34:33 +0530

    Final?

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.listen(process.env.PORT, () => {
   {
     console.log(`Listening to PORT ${process.env.PORT}`);
